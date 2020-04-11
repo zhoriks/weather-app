2465d82cf63f2ccf795814906f5963a00503b05a
Georgy Pogorelov <zhpogorelov@yandex.ru>
2020-04-12 00:04:28 +0500

    Updates

---
+++ precache-manifest.0d86456f3caf614eca6b476f99234d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e986a99cbebf952401137d7403ea0c2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d8ec44faae678c3c5c29",
+    "url": "/weather-app/static/css/main.ecf7c81d.chunk.css"
+  },
+  {
+    "revision": "be49c2815a65db7d608f",
+    "url": "/weather-app/static/js/2.fb834e26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.fb834e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ec44faae678c3c5c29",
+    "url": "/weather-app/static/js/main.aa08f90b.chunk.js"
+  },
+  {
+    "revision": "80a71bacf6ca8d094b30",
+    "url": "/weather-app/static/js/runtime-main.01d02953.js"
+  }
+]);
